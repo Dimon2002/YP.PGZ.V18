13f95d96d91e8a9bf3292c7b82793c6b370edab5
Dimon2002 <91614051+Dimon2002@users.noreply.github.com>
2023-04-04 21:15:11 +0700

    Добавьте файлы проекта.

---
+++ YP.PGZ.System.Time/YP.PGZ.System.Time.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+
+using namespace std;
+
+DWORD  __stdcall ThreadFunc(void*);
+
+DWORD __stdcall ThreadFunc(void*)
+{
+	SYSTEMTIME st;
+	GetSystemTime(&st);
+
+	cout << "Current year: " << st.wYear << endl;;
+
+	exit(EXIT_SUCCESS);
+}
+
+int main()
+{
+	HANDLE StreamDescriptor = nullptr;
+	DWORD StreamID;
+
+	StreamDescriptor = CreateThread(NULL, 0, ThreadFunc, NULL, 0 /*CREATE_SUSPENDED*/, &StreamID);
+
+	if (StreamDescriptor)
+	{
+		WaitForSingleObject(StreamDescriptor, 5); // Почему-то если поставить INFINITY вторым параметром поток завершается прежде чем будет осуществлен вывод текущего года на консоль
+		CloseHandle(StreamDescriptor);
+
+		return EXIT_SUCCESS;
+	}
+
+	cout << GetLastError();
+
+	return EXIT_FAILURE;
+}
